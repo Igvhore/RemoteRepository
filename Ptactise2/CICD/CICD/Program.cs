601fb15eae2c9d8aa4b18f7a75c5c5f51c1a28f6
Игорь Алиев <aliev.igor1@mail.ru>
2024-10-22 13:34:37 +0300

    Создан проект для тестирования CI/CD

---
+++ Ptactise2/CICD/CICD/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CICD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine()), b = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine($"{a/b}");
+        }
+    }
+}
